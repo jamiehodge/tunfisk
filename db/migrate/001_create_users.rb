9270945bff6bdbfbdb502b1cbae501bf12d8f35d
jamiehodge <jamiehodge@me.com>
2012-07-09 16:28:47 +0200

    Username is not null

--- db/migrate/001_create_users.rb
+++ db/migrate/001_create_users.rb
@@ -3,11 +3,9 @@ Sequel.migration do
     create_table :users do
       primary_key :id
       
-      String  :username
+      String  :username, null: false, unique: true
       
       check { :username.like(/^[a-z]{3}\d{3}$/) }
-      
-      unique :username
     end
   end
 end
